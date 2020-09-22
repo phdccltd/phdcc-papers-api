edac0ddd6202d47b89e2fdee4583d2e63ea06450
chriscant <chris@phdcc.com>
2020-09-22 09:48:14 +0100

    Add flowstatus owneradvice

--- models/flowstatuses.js
+++ models/flowstatuses.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     status: { type: Sequelize.STRING(50), allowNull: false },
     ended: { type: DataTypes.BOOLEAN, allowNull: false },
     visibletoauthor: { type: DataTypes.BOOLEAN, allowNull: false },
+    owneradvice: { type: Sequelize.STRING, allowNull: false },            // Shown to owner if this is the most recent status
     // Don't bother making as FK:
     submittedflowstageId: { type: Sequelize.INTEGER, allowNull: true },  // If this type of entry submitted, add this status
     cansubmitflowstageId: { type: Sequelize.INTEGER, allowNull: true },  // If at this status, then this stage can be submitted
