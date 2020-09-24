ed697fe666265c538aa6278621d545aa09a4e690
chriscant <chris@phdcc.com>
2020-09-24 13:20:46 +0100

    Work on downloads

--- routes/downloads.js
+++ routes/downloads.js
@@ -1,3 +1,5 @@
+// Zip files made using https://www.archiverjs.com/
+
 const { Router } = require('express')
 const fs = require('fs')
 const path = require('path')
@@ -80,20 +82,10 @@ router.get('/downloads/anon/:pubid', async function (req, res, next) {
     await writeAnon
 
     // Send file
-    const ContentType = mime.lookup(outpath)
-    var options = {
-      root: TMPDIR,
-      dotfiles: 'deny',
-      headers: {
-        'Content-Type': ContentType,
-        'Content-Disposition': 'attachment; filename="' + saveFilename + '"',
-        'Access-Control-Expose-Headers': 'Content-Disposition',
-      }
-    }
-    res.sendFile(saveFilename, options)
+    sendFile(res, saveFilename)
 
     // Delete tmp file after send
-    setTimeout(() => { fs.unlink(outpath, err => { if (err) console.log(err) }) }, 1000)
+    setTimeout(() => { deleteTempFiles(outpath, false) }, 1000)
   } catch (e) {
     utils.giveup(req, res, e.message)
   }
@@ -104,26 +96,52 @@ router.get('/downloads/anon/:pubid', async function (req, res, next) {
 /* ACCESS: OWNER-ONLY NOT TESTED */
 router.get('/downloads/summary/:pubid', async function (req, res, next) {
   const pubid = parseInt(req.params.pubid)
-  console.log('GET /downloads/summary', pubid)
+  const flowstageid = parseInt(req.query.flowstageid)
+  console.log('GET /downloads/summary', pubid, flowstageid)
   try {
-    // https://www.archiverjs.com/
+    req.dbpub = await models.pubs.findByPk(pubid)
+    if (!req.dbpub) return utils.giveup(req, res, 'Cannot find pubid ' + pubid)
+
+    if (!await dbutils.getMyRoles(req)) return 'No access to this publication'
+
+    if (!req.isowner) return utils.giveup(req, res, 'Not an owner')
+
+    const dbflowstage = await models.flowstages.findByPk(flowstageid)
+    if (!dbflowstage) return utils.giveup(req, res, 'Cannot find dbflowstage')
+
+    const dbflow = await dbflowstage.getFlow()
+    if (!dbflow) return utils.giveup(req, res, 'Cannot find dbflow')
+
+    const dbpubcheck = await dbflow.getPub()
+    if (!dbpubcheck) return utils.giveup(req, res, 'Cannot find dbpubcheck')
+    if (dbpubcheck.id !== req.dbpub.id) return utils.giveup(req, res, 'dbpubcheck.id and dbpub.id mismatch')
+
+    ////////////////
 
     const now = new Date()
-    const dirName = 'summary-' + now.toISOString().substring(0, 16).replace(/:/g, "-")
+    const nowstring = now.toISOString().substring(0, 16).replace(/:/g, "-")
+    const dirName = 'summary-' + nowstring
     fs.mkdirSync(TMPDIR + dirName, { recursive: true })
-    fs.mkdirSync(TMPDIR + dirName+'/papers', { recursive: true })
+    fs.mkdirSync(TMPDIR + dirName + '/papers', { recursive: true })
 
-    const writeFile1 = new Promise((resolve, reject) => {
-      const stream = fs.createWriteStream(TMPDIR + dirName +'/papers/hello.txt')
-      stream.on('close', function (fd) {
-        resolve()
-      })
-      stream.on('open', async function (fd) {
-        stream.write('Hello\rthere\r\r')
-        stream.end()
-      })
-    })
-    await writeFile1
+    ////////
+    const flowstageFilename = dbflowstage.name.replace(/\s/g, "-") + nowstring + '.csv'
+    console.log('flowstageFilename', flowstageFilename)
+    const flowstageStream = await createFile(TMPDIR + dirName + '/' + flowstageFilename)
+    console.log('flowstageStream opened')
+    flowstageStream.write('Scrufulopus\rnow\r\r')
+    await closeFile(flowstageStream)
+    console.log('flowstageStream closed')
+
+    const dbsubmits = await dbflow.getSubmits()
+    for (const dbsubmit of dbsubmits) {
+      console.log('Submit: ', dbsubmit.id)
+    }
+
+    const wf1Stream = await createFile(TMPDIR + dirName + '/papers/hello.txt')
+    wf1Stream.write('Hello\rthere\r\r')
+    await closeFile(wf1Stream)
+
     const writeFile2 = new Promise((resolve, reject) => {
       const stream = fs.createWriteStream(TMPDIR + dirName + '/there.txt')
       stream.on('close', function (fd) {
@@ -137,84 +155,109 @@ router.get('/downloads/summary/:pubid', async function (req, res, next) {
     await writeFile2
 
 
-    const saveFilename = dirName + '.zip'
-
+    // Make zip file from directory
+    const saveFilename = await makeZipOfDirectory(dirName)
     const outpath = path.join(TMPDIR, saveFilename)
 
-    const saveSummaryZip = new Promise((resolve, reject) => {
-      const output = fs.createWriteStream(TMPDIR + saveFilename)
-      const archive = archiver('zip', {
-        zlib: { level: 9 } // Sets the compression level.
-      })
+    // Send Zip file
+    sendFile(res, saveFilename)
 
-      output.on('close', function () { // archive.pointer() has byte count
-        resolve()
-      })
-
-      // good practice to catch warnings (ie stat failures and other non-blocking errors)
-      archive.on('warning', function (err) {
-        if (err.code === 'ENOENT') {
-          console.log('archive warning warning',err)
-        } else {
-          console.log('archive warning error', err)
-          throw err
-        }
-      })
-
-      // good practice to catch this error explicitly
-      archive.on('error', function (err) {
-        console.log('archive error', err)
-        reject()
-      })
-
-      // pipe archive data to the file
-      archive.pipe(output)
-
-      // append a file from stream
-      //const file1 = __dirname + '/file1.txt'
-      //archive.append(fs.createReadStream(file1), { name: 'file1.txt' })
-
-      // append a file from string
-      archive.append('tough tammy', { name: 'file1.txt' })
-      archive.append('string cheese!', { name: 'file2.txt' })
-
-      archive.directory(TMPDIR + dirName, false)
-      
-      // finalize the archive (ie we are done appending files but streams have to finish yet)
-      // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
-      archive.finalize()
-    })
-    await saveSummaryZip
-
-    const ContentType = mime.lookup(saveFilename)
-    var options = {
-      root: TMPDIR,
-      dotfiles: 'deny',
-      headers: {
-        'Content-Type': ContentType,
-        'Content-Disposition': 'attachment; filename="' + saveFilename + '"',
-        'Access-Control-Expose-Headers': 'Content-Disposition',
-      }
-    }
-    res.sendFile(saveFilename, options)
-
-    // Delete tmp file 1 second after send
-    setTimeout(() => {
-      fs.unlink(outpath, err => { if (err) console.log(err) })
-
-      try {
-        deleteFolderRecursivelySync(TMPDIR+dirName)
-      } catch (err) {
-        console.log(err);
-      }
-
-
-
-    }, 1000)
+    // Delete tmp file and directory 1 second after send
+    setTimeout(() => { deleteTempFiles(outpath, dirName) }, 1000)
   } catch (e) {
     utils.giveup(req, res, e.message)
   }
 })
+
+/* ************************ */
+function createFile(path) {
+  return new Promise((resolve, reject) => {
+    const stream = fs.createWriteStream(path)
+    stream.on('open', async function (fd) {
+      resolve(stream)
+    })
+  })
+}
+/* ************************ */
+function closeFile(stream) {
+  return new Promise((resolve, reject) => {
+    stream.on('close', function (fd) {
+      resolve()
+    })
+    stream.end()
+  })
+}
+
+/* ************************ */
+function sendFile(res, saveFilename) {
+  const ContentType = mime.lookup(saveFilename)
+  var options = {
+    root: TMPDIR,
+    dotfiles: 'deny',
+    headers: {
+      'Content-Type': ContentType,
+      'Content-Disposition': 'attachment; filename="' + saveFilename + '"',
+      'Access-Control-Expose-Headers': 'Content-Disposition',
+    }
+  }
+  res.sendFile(saveFilename, options)
+}
+
+/* ************************ */
+function deleteTempFiles(outpath, dirName) {
+  fs.unlink(outpath, err => { if (err) console.log(err) })
+  if (dirName) {
+    deleteFolderRecursivelySync(TMPDIR + dirName)
+  }
+}
+
+/* ************************ */
+async function makeZipOfDirectory(dirName) {
+
+  const saveFilename = dirName + '.zip'
+
+  const saveSummaryZip = new Promise((resolve, reject) => {
+    const output = fs.createWriteStream(TMPDIR + saveFilename)
+    const archive = archiver('zip', {
+      zlib: { level: 9 } // Sets the compression level.
+    })
+
+    output.on('close', function () { // archive.pointer() has byte count
+      resolve()
+    })
+
+    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+    archive.on('warning', function (err) {
+      if (err.code === 'ENOENT') {
+        console.log('archive warning warning', err)
+      } else {
+        console.log('archive warning error', err)
+        throw err
+      }
+    })
+
+    // good practice to catch this error explicitly
+    archive.on('error', function (err) {
+      console.log('archive error', err)
+      reject()
+    })
+
+    // pipe archive data to the file
+    archive.pipe(output)
+
+    // Add all files in directory to ZIP, recurseively
+    archive.directory(TMPDIR + dirName, false)
+
+    // finalize the archive (ie we are done appending files but streams have to finish yet)
+    // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+    archive.finalize()
+  })
+  await saveSummaryZip
+
+  return saveFilename
+}
+
+/* ************************ */
 
 function deleteFolderRecursivelySync(dirpath) {
   const files = fs.readdirSync(dirpath)
@@ -228,5 +271,7 @@ function deleteFolderRecursivelySync(dirpath) {
   })
   fs.rmdirSync(dirpath)
 }
+
+/* ************************ */
 
 module.exports = router
