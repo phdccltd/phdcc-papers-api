d87e22a595aed138b7cb077ca692269a6cab3f9d
edwh <edward@ehibbert.org.uk>
2025-11-13 22:30:01 +0000

    Debug: Switch to database logger for flow actions debugging

--- routes/dbutils.js
+++ routes/dbutils.js
@@ -59,24 +59,25 @@ async function getMyRoles (req) {
 
   req.dbmypubroles = await req.dbuser.getRoles()
   req.myroles = []
-  console.log('DEBUG getMyRoles: user=', req.dbuser.id, req.dbuser.name, 'pub=', req.dbpub.id, 'total roles=', req.dbmypubroles.length)
+  const logger = require('../logger')
+  logger.log('GETMYROLES_DEBUG', `user=${req.dbuser.id}`, `name=${req.dbuser.name}`, `pub=${req.dbpub.id}`, `totalroles=${req.dbmypubroles.length}`)
   for (const dbmypubrole of req.dbmypubroles) {
     if (dbmypubrole.pubId === req.dbpub.id) {
       // console.log('GETMYROLES', dbmypubrole)
       const mypubrole = models.sanitise(models.pubroles, dbmypubrole)
       req.myroles.push(mypubrole)
-      console.log('DEBUG getMyRoles: Found role for pub', req.dbpub.id, ':', mypubrole.name, 'defaultrole=', mypubrole.defaultrole, 'isowner=', mypubrole.isowner)
+      logger.log('GETMYROLES_DEBUG', `role=${mypubrole.name}`, `defaultrole=${mypubrole.defaultrole}`, `isowner=${mypubrole.isowner}`)
       if (mypubrole.isowner) req.isowner = true
       if (mypubrole.canviewall) req.canviewall = true
       if (mypubrole.defaultrole) { // ie author
         req.onlyanauthor = true
         req.isauthor = true
-        console.log('DEBUG getMyRoles: Set isauthor=true for user', req.dbuser.id)
+        logger.log('GETMYROLES_DEBUG', `SET_ISAUTHOR=true for user ${req.dbuser.id}`)
       }
     }
   }
   if (req.myroles.length >= 2) req.onlyanauthor = false
-  console.log('DEBUG getMyRoles: Final - isauthor=', req.isauthor, 'isowner=', req.isowner, 'roles count=', req.myroles.length)
+  logger.log('GETMYROLES_DEBUG', `FINAL isauthor=${req.isauthor}`, `isowner=${req.isowner}`, `rolescount=${req.myroles.length}`)
 
   return true
 }
--- routes/submits.js
+++ routes/submits.js
@@ -1061,17 +1061,18 @@ async function getPubSubmits (req, res, next) {
 
       /// /////// Set up flow-level actions that are possible
       flow.actions = [] // Allowable actions
-      console.log('DEBUG: Setting up flow actions for user', req.dbuser.id, req.dbuser.name, 'isauthor=', req.isauthor, 'acceptings=', flow.acceptings.length)
+      const logger = require('../logger')
+      logger.log('FLOW_ACTIONS_DEBUG', `user=${req.dbuser.id}`, `name=${req.dbuser.name}`, `isauthor=${req.isauthor}`, `acceptings=${flow.acceptings.length}`)
       for (const accepting of flow.acceptings) {
         if (_.isNull(accepting.flowstatusId) && accepting.open) {
           const addstage = _.find(flow.stages, stage => { return stage.id === accepting.flowstageId })
-          console.log('DEBUG: Checking accepting - stage=', addstage?.name, 'isauthor=', req.isauthor)
+          logger.log('FLOW_ACTIONS_DEBUG', `stage=${addstage?.name}`, `isauthor=${req.isauthor}`)
           if (addstage && req.isauthor) {
             flow.actions.push({
               name: 'Add new ' + addstage.name,
               route: '/panel/' + pubid + '/' + flow.id + '/add/' + addstage.id
             })
-            console.log('DEBUG: Added action:', 'Add new ' + addstage.name)
+            logger.log('FLOW_ACTIONS_DEBUG', `ADDED_ACTION: Add new ${addstage.name}`)
           }
         }
       }
